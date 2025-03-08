a6de8264619f483a55344f2358371dbb57a253aa
lizzone660 <chescoburton660@gmail.com>
2025-03-08 17:35:53 +0300

    Oky

---
+++ script.js
@@ -0,0 +1,13 @@
+function show() {
+    document.getElementById("ans1").style.display = "block"
+}
+
+
+function show2() {
+    document.getElementById("ans2").style.display = "block"
+}
+
+
+function show3() {
+    document.getElementById("ans3").style.display = "block"
+}
